59fec59e548a37946a256d50a8c6b45c936a0921
Ben <bsmith20681@gmail.com>
2021-01-07 20:33:40 -0700

    created and added image to header

--- New Portfolio/portfolio/src/pages/index.js
+++ New Portfolio/portfolio/src/pages/index.js
@@ -2,20 +2,23 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+import Memoji from "../images/memoji.png"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6">
+          Hi I'm Ben Smith. I am a Front End web developer.
+        </div>
 
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+        <div className="col-md-6">
+          <img src={Memoji}></img>
+        </div>
+      </div>
     </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
 )
