112bfe09aa956b6fa97906a1af1bf274ce413037
Ben <bsmith20681@gmail.com>
2021-01-23 17:35:04 -0700

    added emailjs functionality now sends email when contact form is submitted

--- New Portfolio/portfolio/src/pages/contact-me.js
+++ New Portfolio/portfolio/src/pages/contact-me.js
@@ -1,51 +1,109 @@
-import React from "react"
+import React, { useState } from "react"
 import * as emailjs from "emailjs-com"
 //import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const ContactMe = () => (
-  <Layout>
-    <SEO title="Home" />
-    <div className="container">
-      <div style={{ textAlign: "center" }}>
-        <h1>Contact Me</h1>
-        <p>
-          This message goes to my personal email and I usually respond within 24
-          hours
-        </p>
+const ContactMe = () => {
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [message, setMessage] = useState("")
+  const [messageSuccess, setMessageSuccess] = useState(null)
+
+  const handleSubmit = e => {
+    e.preventDefault()
+
+    emailjs
+      .sendForm(
+        "service_zr5224k",
+        "template_u6vwy56",
+        e.target,
+        "user_cUDGxCajC3J07prrCf4OQ"
+      )
+      .then(
+        result => {
+          console.log(result.text)
+          setMessageSuccess(true)
+        },
+        error => {
+          console.log(error.text)
+          setMessageSuccess(false)
+        }
+      )
+
+    setName("")
+    setEmail("")
+    setMessage("")
+  }
+
+  const messageSent = () => {
+    return <h1>Your message was sent</h1>
+  }
+
+  const messageFail = () => {
+    return <h1>Your message was NOT sent</h1>
+  }
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <div className="container">
+        <div style={{ textAlign: "center" }}>
+          <h1>Contact Me</h1>
+          <p>
+            This message goes to my personal email and I usually respond within
+            24 hours
+          </p>
+        </div>
+        <div>
+          <form onSubmit={handleSubmit} className="contact-form">
+            <div className="row">
+              <div className="col-md-6">
+                <input
+                  type="text"
+                  className="contact-input"
+                  placeholder="Name"
+                  value={name}
+                  name="name"
+                  onChange={e => setName(e.target.value)}
+                />
+              </div>
+              <div className="col-md-6">
+                <input
+                  type="text"
+                  className="contact-input"
+                  placeholder="Email"
+                  value={email}
+                  name="email"
+                  onChange={e => setEmail(e.target.value)}
+                />
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-12">
+                <textarea
+                  className="contact-textArea"
+                  rows="10"
+                  placeholder="Type Your Message..."
+                  value={message}
+                  name="message"
+                  onChange={e => setMessage(e.target.value)}
+                ></textarea>
+              </div>
+            </div>
+            <button
+              type="submit"
+              className="btn btn-blue-filled"
+              style={{ border: "none" }}
+            >
+              Send Message
+            </button>
+          </form>
+        </div>
       </div>
-      <div>
-        <form action="" className="contact-form">
-          <div className="row">
-            <div className="col-md-6">
-              <input type="text" className="contact-input" placeholder="Name" />
-            </div>
-            <div className="col-md-6">
-              <input
-                type="email"
-                className="contact-input"
-                placeholder="Email"
-              />
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-md-12">
-              <textarea
-                className="contact-textArea"
-                rows="10"
-                placeholder="Type Your Message..."
-              ></textarea>
-            </div>
-          </div>
-          <button className="btn btn-blue-filled" style={{ border: "none" }}>
-            Send Message
-          </button>
-        </form>
-      </div>
-    </div>
-  </Layout>
-)
+    </Layout>
+  )
+}
 
 export default ContactMe
