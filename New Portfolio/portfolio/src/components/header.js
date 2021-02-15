d33479c00243c246fe59b4be4203a524dcd90e74
Ben <bsmith20681@gmail.com>
2021-02-14 17:37:05 -0700

    added styles to blog post page

--- New Portfolio/portfolio/src/components/header.js
+++ New Portfolio/portfolio/src/components/header.js
@@ -1,35 +1,46 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
 import React from "react"
+import { useState } from "react"
+import { Link } from "gatsby"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div className="container">
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `#333333`,
-            textDecoration: `none`,
-          }}
-        >
-          ASK<span style={{ color: "#007BFF" }}>BENSMITH</span>.COM
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
+const Header = props => {
+  const [mobileMenu, setMobileMenu] = useState(false)
+  const toggleMenu = e => setMobileMenu(!mobileMenu)
+  return (
+    <header className="container">
+      {/* Actual Menu*/}
+      <nav>
+        <h3>
+          <Link to="/">logo</Link>
+        </h3>
+        <ul className={"nav-link-list " + (mobileMenu ? "open" : "")}>
+          <li className="nav-link">
+            <a href="">Home</a>
+          </li>
+          <li className="nav-link">
+            <a href="">About Us</a>
+          </li>
+          <li className="nav-link">
+            <a href="">Contact Us</a>
+          </li>
+        </ul>
+      </nav>
+      {/*Mobile Menu Utilities*/}
+      <div id="hamburger" onClick={toggleMenu}>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+      <div
+        id="close-menu"
+        className={mobileMenu ? "open" : ""}
+        onClick={toggleMenu}
+      >
+        <div></div>
+        <div></div>
+      </div>
+      <a href="#" id={mobileMenu ? "overlay" : ""} onClick={toggleMenu}></a>
+    </header>
+  )
 }
 
 export default Header
